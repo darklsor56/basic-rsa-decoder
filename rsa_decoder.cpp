ae999575f8015e62ef11fe3a88c2489f9830e935
Daniel Remedios <dmr42203@gmail.com>
2025-04-26 19:47:55 -0700

    Added the functionality of finding p and q as well as added a .gitignore for my convienence

--- rsa_decoder.cpp
+++ rsa_decoder.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
+#include<array>
+#include<cmath>
 
 using std::cout;
 using std::endl;
 
-int e = 7;              // Relatively prime with $\phi (n)$
-int n = 4453;           // n = p * q
-const int m = 115;      // The number of characters in the message
+int main() {
+    const int e = 7;              // Relatively prime with $\phi (n)$
+    const int n = 4453;           // n = p * q
+    const int m = 115;            // The number of characters in the message
+    int p;                        // The prime number that makes up n
+    int q;                        // The prime number that makes up n
+    int phi;                      // $\phi (n) = (p-1)(q-1)$
+    int M;                        // A decrypted node
+    std::array<int, 16> primes = {7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67};
 
-cout << "Hello! Alice has given us Bob's e = 7 and n = 4453" << endl;
 
+    cout << "Hello! Alice has given us Bob's e and n values and she expects us to edcrypt Bob's message" << endl;
+    cout << "in a reasonable amount of time with this program." << endl;
+    cout << endl;
+    cout << "First, let's get the prime numbers that make up n, the prime integers p and q..." << endl;
+    cout << endl;
+
+    // Iterate through the list of possible factors of prime numbers as shown earlier in LaTeX
+    for(unsigned int i = 0; i < primes.size(); ++i) {
+        if (n % primes[i] == 0) {
+            p = primes[i];
+            q = n / primes[i];
+        }
+    }
+    return 0;
+}
