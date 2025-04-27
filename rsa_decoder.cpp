7b742c4597f953b9881d89822b580a70013c47ec
Daniel Remedios <dmr42203@gmail.com>
2025-04-26 21:05:09 -0700

    Finished up the program a bit. First check with gradescope.

--- rsa_decoder.cpp
+++ rsa_decoder.cpp
@@ -5,6 +5,10 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
+
+// The key to plaintext decoder
+char key[38];
 
 // Inverse Check
 int modularInverse(int e, int phi) {
@@ -52,14 +56,41 @@ bool findFactors(int n, int& p, int& q) {
     return false;
 }
 
+int decypher(int base, int exponent, int mod) {
+    int result = 1;
+    base = base % mod;
+
+    while(exponent > 0) {
+        if(exponent % 2 == 1) { //check if odd
+            result = (1LL * result * base) % mod; //Use Long-Long (LL)
+        }
+        base = (1LL * base * base) % mod;
+        exponent = exponent / 2;
+    }
+
+    return result;
+}
+
+void buildKey() {
+    for(unsigned int i = 7; i <= 32; ++i) {
+        key[i] = 'A' + (i - 7); //Should increment through alphabet correctly
+    }
+    key[33] = ' ';
+    key[34] = '"';
+    key[35] = ',';
+    key[36] = '.';
+    key[37] = '\'';
+}
+
 int main() {
-    int e = 7;              // Relatively prime with $\phi (n)$
-    int n = 4453;           // n = p * q
-    int m = 115;            // The number of characters in the message
+    int e;              // Relatively prime with $\phi (n)$
+    int n;           // n = p * q
+    int m;            // The number of characters in the message
     int p = 0;                        // The prime number that makes up n
     int q = 0;                        // The prime number that makes up n
     int phi = 0;                      // $\phi (n) = (p-1)(q-1)$
     int M;                        // A decrypted node
+    /*
     std::array<int, 115> tempM = {1400, 2218, 99, 2970, 2218, 2962, 3015, 99, 871, 843,
         3780, 843, 84, 2218, 2269, 2218, 1443, 2962, 99, 84,
         843, 1655, 2979, 99, 843, 3015, 2382, 447, 4191, 871,
@@ -72,9 +103,9 @@ int main() {
         843, 1655, 99, 1655, 1443, 1443, 3237, 99, 4191, 2962,
         3015, 99, 1655, 843, 4191, 3015, 99, 4191, 99, 4242,
         1443, 1443, 3629, 2916, 1400};
+    */
     // n is probably randomized... AAAAAAHHHHH
     //std::array<int, 18> primes = {2, 3, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67};
-
 
     //cout << "Hello! Alice has given us Bob's e and n values and she expects us to edcrypt Bob's message" << endl;
     //cout << "in a reasonable amount of time with this program." << endl;
@@ -84,33 +115,44 @@ int main() {
 
     // Iterate through the list of possible factors of prime numbers as shown earlier in LaTeX
     
+    // build key
+    cin >> e;
+    cin >> n;
+    cin >> m;
+    buildKey();
 
     if(!findFactors(n, p, q)) {
-        throw std::runtime_error("ERROR: Failed to find p or q");
+        //throw std::runtime_error("ERROR: Failed to find p or q");
+        cout << "Public key is not valid!";
         return 1;
     }
-
-    cout << "p = " << p << ", q = " << q << endl;
     //cout << "Now let's compute phi(n)" << endl;
     //cout << endl;
 
     phi = (p-1)*(q-1);
-
-    cout << "phi(n) = " << phi << endl;
     //cout << "Now let's compute d such that d*e congruent to 1 mod 4320"
 
     // Use the Extended Euclidean Algorithm or Brute Force!
     int d = modularInverse(e, phi);
     if(d == -1) {
-        throw std::runtime_error("ERROR: given e is not invertable suggesting public key is not valid!");
+        //throw std::runtime_error("ERROR: given e is not invertable suggesting public key is not valid!");
+        cout << "Public key is not valid!";
         return 1;
     }
 
-    cout << "d = " << d << endl;
-
     std::string message = "";
 
-    
+    int C;
+
+    for(unsigned int i = 0; i < m; ++i) {
+        cin >> C;
+        M = decypher(C, d, n);
+
+        cout << M << " ";
+        message += key[M];
+    }
+    cout << endl;
+    cout << message << endl;
 
     return 0;
 }
