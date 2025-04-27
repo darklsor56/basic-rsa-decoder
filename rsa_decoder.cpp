1d2201541da13b09893930f9799ae0de1a6b7e7e
Daniel Remedios <dmr42203@gmail.com>
2025-04-26 19:18:46 -0700

    The whole program will be stored here. Made variables and an intro

---
+++ rsa_decoder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+int e = 7;              // Relatively prime with $\phi (n)$
+int n = 4453;           // n = p * q
+const int m = 115;      // The number of characters in the message
+
+cout << "Hello! Alice has given us Bob's e = 7 and n = 4453" << endl;
+
