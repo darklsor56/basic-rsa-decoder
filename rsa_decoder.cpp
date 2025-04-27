5c994cbe1210b1a9b89b01f43e9eebdc1bef1a08
Daniel Remedios <dmr42203@gmail.com>
2025-04-26 20:12:25 -0700

    Added Extended Euclidean Algorithm to solve for d

--- rsa_decoder.cpp
+++ rsa_decoder.cpp
@@ -5,6 +5,32 @@
 using std::cout;
 using std::endl;
 
+// Inverse Check
+int modularInverse(int e, int phi) {
+    int t = 0, new_t = 1;       // coefficients for e for backtracking
+    int r = phi, new_r = e;     // Renainders
+    
+    while (new_r != 0) {
+        int quotient = r / new_r;
+
+        int temp = new_t;
+        new_t = t - quotient * new_t;
+        t = temp;
+
+        temp = new_r;
+        new_r = r - quotient * new_r;
+        r = temp;
+    }
+
+    if (r > 1) {
+        return -1; // e is not invertible
+    }
+    if (t < 0) {
+        t += phi;
+    }
+    return t;
+}
+
 int main() {
     const int e = 7;              // Relatively prime with $\phi (n)$
     const int n = 4453;           // n = p * q
@@ -37,14 +63,20 @@ int main() {
 
     cout << "p = " << p << ", q = " << q << endl;
     //cout << "Now let's compute phi(n)" << endl;
-    cout << endl;
+    //cout << endl;
 
     phi = (p-1)*(q-1);
 
     cout << "phi(n) = " << phi << endl;
     //cout << "Now let's compute d such that d*e congruent to 1 mod 4320"
 
+    // Use the Extended Euclidean Algorithm or Brute Force!
+    int d = modularInverse(e, phi);
+    if(d == -1) {
+        std::runtime_error("ERROR: given e is not invertable suggesting public key is not valid!");
+    }
 
+    cout << "d = " << d << endl;
 
     return 0;
 }
